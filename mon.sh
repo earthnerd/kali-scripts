3875d5f9f8519ab9d9d319c599bab2d394a0a7c6
earthnerd <luke543.github@gmail.com>
2016-06-15 17:02:46 +1000

    Update mon.sh

--- mon.sh
+++ mon.sh
@@ -1,5 +1,3 @@
-  GNU nano 2.5.3                                   File: mon.sh                                                                             
-
 #!/bin/bash
 
 # monitor mode by earthnerd
@@ -16,14 +14,14 @@
 #############################################################
 
 
-title=$(/bin/echo -e "                     \e[1;30m_                                    _       
+title=$(/bin/echo -e "  	   	   \e[1;30m_                                    _       
                   (_)_                                 | |      
  ____   ___  ____  _| |_  ___   ____    ____   ___   _ | | ____ 
 |    \ / _ \|  _ \| |  _)/ _ \ / ___)  |    \ / _ \ / || |/ _  )
 | | | | |_| | | | | | |_| |_| | |      | | | | |_| ( (_| ( (/ / 
 |_|_|_|\___/|_| |_|_|\___)___/|_|      |_|_|_|\___/ \____|\____)
 
-                                                   by earthnerd\e[0m
+						   by earthnerd\e[0m
 ")
 
 clear
@@ -34,6 +32,43 @@ echo "$title
 
 ### kills interfering processes, puts interface
 ### into monitor mode and brings it down
-                                                             [ Read 74 lines ]
-^G Get Help    ^O Write Out   ^W Where Is    ^K Cut Text    ^J Justify     ^C Cur Pos     ^Y Prev Page   M-\ First Line M-W WhereIs Next
-^X Exit        ^R Read File   ^\ Replace     ^U Uncut Text  ^T To Linter   ^_ Go To Line  ^V Next Page   M-/ Last Line  M-] To Bracket
+###############################################
+
+/bin/echo -e "\e[1;33m|--------------------------------------------|
+|--- Killing processes that may interfere ---|
+|--------------------------------------------|\e[0m"
+
+
+airmon-ng check kill >/dev/null
+
+
+/bin/echo -e "\e[1;33m|--- Putting wlan0 into monitor mode --------|
+|--------------------------------------------|\e[0m"
+
+
+airmon-ng start wlan0 >/dev/null
+
+ifconfig wlan0mon down
+
+
+# changing mac
+##############
+
+/bin/echo -e "\e[1;33m|--- Here is your new mac address -----------|
+|--------------------------------------------|\e[0m"
+
+newmac=$(macchanger -a wlan0mon | grep New)
+
+/bin/echo -e "
+\e[1;32m$newmac\e[0m
+"
+
+
+# bringing device back up
+#########################
+
+/bin/echo -e "\e[1;33mInterface is now called\e[0m \e[1;31mwlan0mon\e[0m
+"
+/bin/echo -e "\e[1;33mHave a nice day :)\e[0m
+
+"
